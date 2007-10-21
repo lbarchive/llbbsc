d58992629c1e11d2ddf28f127daebe4bfc727718
livibetter <livibetter@04f9d565-bf3c-0410-8b27-f347a6cfe449>
2007-10-21 04:53:21 +0000

    Added: GPL statement

--- trunk/bbPress/OnThisDay/on-this-day.php
+++ trunk/bbPress/OnThisDay/on-this-day.php
@@ -8,6 +8,22 @@ Author URI: http://www.livibetter.com/
 Version: 0.1
 Creation Date: 2007-10-19 09:11:33 UTC+8
 */
+/*
+ * Copyright 2007 Yu-Jie Lin
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 function OTDHook_bb_view_otd_where($where) {
     $key = 'YEAR(t.topic_start_time) ';
